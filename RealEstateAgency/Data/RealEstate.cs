e0fdde1ffac8e6090abb3a88d5610fbf6a64d574
dudwa <dorian.dudas@gmail.com>
2023-11-14 16:09:17 +0100

    fix: Rename the Property class to RealEstate class

---
+++ RealEstateAgency/Data/RealEstate.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using RealEstateAgency.Model;
+
+namespace RealEstateAgency.Data;
+
+public class RealEstate
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; init; }
+    public int UserId { get; init; }
+    public PropertyType Type { get; init; }
+    public string City { get; init; }
+    public string Address { get; init; }
+    public decimal Value { get; set; }
+    public int GroundSpace { get; init; }
+    public int BuildYear { get; init; }
+    public string About { get; set; }
+    public DateTime SubmitDate { get; init; }
+    [JsonIgnore]
+    public bool IsActive { get; set; }
+
+}
