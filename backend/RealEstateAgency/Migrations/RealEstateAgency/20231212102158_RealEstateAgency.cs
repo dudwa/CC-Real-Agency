27ffc2b60e004ddd162892bd30c2bef8abac75ff
dudwa <dorian.dudas@gmail.com>
2023-12-12 11:44:01 +0100

    refactor: Add migrations for updated contexts

---
+++ backend/RealEstateAgency/Migrations/RealEstateAgency/20231212102158_RealEstateAgency.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateAgency.Migrations.RealEstateAgency
+{
+    /// <inheritdoc />
+    public partial class RealEstateAgency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Qnas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Question = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Qnas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RealEstates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<double>(type: "float", nullable: false),
+                    GroundSpace = table.Column<int>(type: "int", nullable: false),
+                    BuildYear = table.Column<int>(type: "int", nullable: false),
+                    About = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SubmitDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RealEstates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Qnas");
+
+            migrationBuilder.DropTable(
+                name: "RealEstates");
+        }
+    }
+}
--- backend/RealEstateAgency/Service/Repository/RealEstateRepository.cs
+++ backend/RealEstateAgency/Service/Repository/RealEstateRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using RealEstateAgency.Data;
 using RealEstateAgency.Model;
 
@@ -5,40 +6,47 @@ namespace RealEstateAgency.Service.Repository;
 
 public class RealEstateRepository : IRealEstateRepository
 {
+    private readonly DbContextOptions<RealEstateAgencyContext> _dbContextOptions;
+
+    public RealEstateRepository(DbContextOptions<RealEstateAgencyContext> dbContextOptions )
+    {
+        _dbContextOptions = dbContextOptions;
+    }
+
     public List<RealEstate> GetAll()
     {
-        using var dbContext = new RealEstateAgencyContext();
+        using var dbContext = new RealEstateAgencyContext(_dbContextOptions);
         return dbContext.RealEstates.ToList();
     }
 
     public List<RealEstate> GetAllForSale()
     {
-        using var dbContext = new RealEstateAgencyContext();
+        using var dbContext = new RealEstateAgencyContext(_dbContextOptions);
         return dbContext.RealEstates.Where(re => re.Status == RealEstateStatus.ForSale).ToList();
     }
 
     public List<RealEstate> GetAllToRent()
     {
-        using var dbContext = new RealEstateAgencyContext();
+        using var dbContext = new RealEstateAgencyContext(_dbContextOptions);
         return dbContext.RealEstates.Where(re => re.Status == RealEstateStatus.ToRent).ToList();
     }
 
     public List<RealEstate> GetAllByUser(int userId)
     {
-        using var dbContext = new RealEstateAgencyContext();
+        using var dbContext = new RealEstateAgencyContext(_dbContextOptions);
         return dbContext.RealEstates.Where(re => re.UserId == userId).ToList();
     }
 
     public void Add(RealEstate realEstate)
     {
-        using var dbContext = new RealEstateAgencyContext();
+        using var dbContext = new RealEstateAgencyContext(_dbContextOptions);
         dbContext.Add(realEstate);
         dbContext.SaveChanges();
     }
 
     public void Delete(int id)
     {
-        using var dbContext = new RealEstateAgencyContext();
+        using var dbContext = new RealEstateAgencyContext(_dbContextOptions);
         RealEstate realEstateToRemove = new RealEstate() { Id = id };
         dbContext.Remove(realEstateToRemove);
         dbContext.SaveChanges();
@@ -46,7 +54,7 @@ public class RealEstateRepository : IRealEstateRepository
 
     public void Update(RealEstate realEstate)
     {
-        using var dbContext = new RealEstateAgencyContext();
+        using var dbContext = new RealEstateAgencyContext(_dbContextOptions);
         var realEstatetoupdate = dbContext.RealEstates.Find(realEstate.Id);
         realEstatetoupdate = realEstate;
         dbContext.SaveChanges();
