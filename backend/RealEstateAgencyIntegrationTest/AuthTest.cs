13370dd161061dbddeaad70a95f73e72d3445da3
dudwa <dorian.dudas@gmail.com>
2023-12-14 17:23:19 +0100

    refactor: Remove the comment lines

--- backend/RealEstateAgencyIntegrationTest/AuthTest.cs
+++ backend/RealEstateAgencyIntegrationTest/AuthTest.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using RealEstateAgency.Data;
 using RealEstateAgency.Service.Authentication;
-using RealEstateAgency.Service.Repository;
 
 namespace RealEstateAgencyIntegrationTest;
 
@@ -24,10 +23,9 @@ public class AuthTest
     public void TestSetUp()
     {
         var webHostBuilder = new WebHostBuilder()
-            .UseStartup<Startup>() // Assuming Startup is your Startup class
+            .UseStartup<Startup>()
             .ConfigureTestServices(services =>
             {
-                // Replace the actual database with an in-memory database for testing
                 var serviceProvider = new ServiceCollection()
                     .AddEntityFrameworkInMemoryDatabase()
                     .BuildServiceProvider();
@@ -55,7 +53,7 @@ public class AuthTest
 
         Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
     }
-    
+
     [TestMethod]
     public async Task PostLoginUsernameMatchesNameClaimInResponseJWT()
     {
@@ -73,7 +71,7 @@ public class AuthTest
             }
         }
     }
-    
+
     [TestMethod]
     public async Task PostQnaWithAdminLoginResultsInCreatedStatusCode()
     {
@@ -82,12 +80,12 @@ public class AuthTest
         var token = await Login(userName, password);
         string addEndpoint = "/Qna/add";
         string addJsonBody = JsonConvert.SerializeObject(new Qna(0, "Is it cloudy outside?", "No, it isn't."));
- 
+
         var response = await SendPostRequestAsync(addEndpoint, addJsonBody, token);
 
         Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
     }
-    
+
     [TestMethod]
     public async Task PostQnaWithUserLoginResultsInCreatedStatusCode()
     {
@@ -96,7 +94,7 @@ public class AuthTest
         var token = await Login(userName, password);
         string addEndpoint = "/Qna/add";
         string addJsonBody = JsonConvert.SerializeObject(new Qna(0, "Is it cloudy outside?", "No, it isn't."));
- 
+
         var response = await SendPostRequestAsync(addEndpoint, addJsonBody, token);
 
         Assert.AreEqual(HttpStatusCode.Forbidden, response.StatusCode);
@@ -106,26 +104,23 @@ public class AuthTest
     {
         using (HttpContent content = new StringContent(jsonBody, Encoding.UTF8, "application/json"))
         {
-            // Add the authorization header if a token is provided
             if (!string.IsNullOrEmpty(token))
             {
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             }
 
-            // Send the POST request
             HttpResponseMessage response = await client.PostAsync(endpoint, content);
 
-            // Reset the authorization header to avoid affecting other requests
             client.DefaultRequestHeaders.Authorization = null;
 
             return response;
         }
     }
-    
+
     private async Task<string> Login(string userName, string password)
     {
         string endpoint = "/Auth/login";
-        string jsonBody = JsonConvert.SerializeObject(new AuthRequest(userName , password));
+        string jsonBody = JsonConvert.SerializeObject(new AuthRequest(userName, password));
         var response = await SendPostRequestAsync(endpoint, jsonBody);
         response.EnsureSuccessStatusCode();
         var responseJson = await response.Content.ReadAsStringAsync();
